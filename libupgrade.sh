09a1b2122bdb1be0fd0cdda91ce917db462fe206
bhaukaalbaba <47282314+bhaukaalbaba@users.noreply.github.com>
2022-10-07 15:39:03 +0530

    Update libupgrade.sh

--- libupgrade.sh
+++ libupgrade.sh
@@ -1,5 +1,5 @@
 systemctl stop cron php7.4-fpm nginx ; 
-apt remove php* -y ;
+apt remove php7.4-* -y ;
 apt autoremove -y ;
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg ;
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-php.list ;
@@ -9,7 +9,7 @@ wget -O /etc/php/8.1/fpm/php.ini https://raw.githubusercontent.com/bhaukaalbaba/
 wget -O /etc/php/8.1/cli/php.ini https://raw.githubusercontent.com/bhaukaalbaba/librenmsconfig/main/cliphp.ini ;
 wget -O /etc/php/8.1/fpm/pool.d/librenms.conf https://raw.githubusercontent.com/bhaukaalbaba/librenmsconfig/main/phpfpmconf.conf ;
 systemctl enable php8.1-fpm ; 
-systemctl start php8.1-fpm nginx cron ; 
-
-
+systemctl start php8.1-fpm ;
+systemctl restart php8.1-fpm nginx cron ; 
+echo "LibreNMS upgrade is complete"
 
